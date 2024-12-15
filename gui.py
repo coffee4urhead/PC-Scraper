e96048a3d6005f3674ebce5ca4469a902152a290
Mihail <dark1982soul@gmail.com>
2024-12-15 23:51:51 +0200

    Adding styles to the gui and making part of the web scraper

--- gui.py
+++ gui.py
@@ -1,24 +1,43 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
+from PIL import Image, ImageTk
+from scraper import begin_scraping
 
-root = Tk()
-frm = ttk.Frame(root, padding=20)
+root = tk.Tk()
+root.title("GUI App for Scraping General GPU Information")
+root.geometry("1200x700")
 
-frm.grid()
+image = Image.open("./images/elf.jpg")
+resized_image = image.resize((1200, 700), Image.Resampling.LANCZOS)
+background_image = ImageTk.PhotoImage(resized_image)
 
-ttk.Label(frm, text="GPU Web Scraper Program").grid(column=0, row=0, columnspan=3, pady=10)
+canvas = tk.Canvas(root, width=1200, height=700, highlightthickness=0)
+canvas.pack(fill="both", expand=True)
+canvas.create_image(0, 0, image=background_image, anchor="nw")
 
-ttk.Label(frm, text="Search GPU model to scrape information:").grid(column=0, row=1, pady=10)
+label1 = tk.Label(root, text="GPU Web Scraper Program", font=("Comic Sans MS", 16, "bold"), )
+label1_window = canvas.create_window(600, 50, anchor="center", window=label1)
 
-entry = ttk.Entry(frm, width=30)
-entry.grid(column=1, row=1, padx=10)
+label2 = tk.Label(root, text="Search GPU model to scrape information:", font=("Comic Sans MS", 12))
+label2_window = canvas.create_window(400, 120, anchor="center", window=label2)
+
+def on_key_press(event):
+    enter_pressed_value = entry.get()
+    begin_scraping(enter_pressed_value)
+
+entry = tk.Entry(root, width=30, font=("Comic Sans MS", 13, "italic"))
+root.bind("<Return>", on_key_press)
+entry_window = canvas.create_window(750, 120, anchor="center", window=entry)
 
 def on_submit():
-    user_input = entry.get()
-    print(f"Submitted text: {user_input}")
+    user_submit_value = entry.get()
+    begin_scraping(user_submit_value)
 
-ttk.Button(frm, text="Submit", command=on_submit).grid(column=2, row=1, padx=10)
+submit_button = tk.Button(root, text="Submit", command=on_submit, width=7, font=("Comic Sans MS", 13, "bold"), height = 1, cursor="hand2", padx=10, pady=5)
+submit_button_window = canvas.create_window(650, 180, anchor="center", window=submit_button)
 
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=2, pady=20)
+quit_button = tk.Button(root, text="Quit", command=root.destroy, width=7, font=("Comic Sans MS", 13, "bold"), height = 1, cursor="hand2", padx=10, pady=5)
+quit_button_window = canvas.create_window(750, 180, anchor="center", window=quit_button)
 
 root.mainloop()
+
+
---
+++ scraper.py
@@ -0,0 +1,39 @@
+import requests
+import time
+from bs4 import BeautifulSoup
+
+
+def begin_scraping(search_term):
+    base_url = f"{search_term}"
+
+    try:
+        response = requests.get(base_url)
+        if response.status_code == 200:
+            time.sleep(2)
+
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            price_tags = soup.find_all("span", class_="a-price")
+
+            if price_tags:
+                for price_tag in price_tags:
+
+                    symbol = price_tag.find("span", class_="a-price-symbol")
+                    whole = price_tag.find("span", class_="a-price-whole")
+                    decimal = price_tag.find("span", class_="a-price-decimal")
+                    fractional = price_tag.find("span", class_="a-price-symbol")
+
+                    if symbol and whole and decimal and fractional:
+                        full_price = f"{symbol.get_text()}{whole.get_text()}{decimal.get_text()}{fractional.get_text()}"
+                        print("Price:", full_price)
+                    else:
+                        print("Price format is missing components.")
+            else:
+                print("No prices found on this page.")
+        else:
+            print(f"Failed to retrieve the page. Status code: {response.status_code}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
